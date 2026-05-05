9769ca825d16819e6e1130531154ac929100d845
Sparrow <voronovskyy.danylo@gmail.com>
2026-05-05 15:49:38 +0200

    Add project files.

---
+++ APBD_Test_s33596/Excpetions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace APBD_Test_s33596.Excpetions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() { }
+        public NotFoundException(string message) : base(message) { }
+    }
+}
